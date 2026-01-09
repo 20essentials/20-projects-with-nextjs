ebad10a9606b582a1d5f093161fa8908a6f4add3
DominicDev <s0l0estudi4r@gmail.com>
2026-01-09 21:40:00 +0100

    v0.2.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,9 @@ export default defineConfig({
   },
 
   //Localhost
-  // site: 'http://localhost:4321/'
+  site: 'http://localhost:4321/'
 
   //Deploy normal project
-  base: '/20-projects-with-nextjs/',
-  site: 'https://20essentials.github.io/20-projects-with-nextjs/'
+  // base: '/20-projects-with-nextjs/',
+  // site: 'https://20essentials.github.io/20-projects-with-nextjs/'
 });
